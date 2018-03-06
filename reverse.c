6d561b622a1457ca3afca3f6657d463fab9dd547
archanakichu123 <archanaabhi3125@gmail.com>
2018-03-06 15:32:24 +0530

    my_first commit

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[40],n,i,j,f=0,count++;
+int index_arr[10];
+printf("Enter an array : ");
+for(i=0;i<n;i++)
+scanf("%d",&a[i];
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(a[i]==a[j])
+{
+index_arr[j]=j;
+}
